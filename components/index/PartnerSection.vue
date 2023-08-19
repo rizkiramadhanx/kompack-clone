6658dd21b926f8132e39fd43ff050f98d8a2b948
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-08-19 09:27:03 +0700

    feat : add line partner

--- components/index/PartnerSection.vue
+++ components/index/PartnerSection.vue
@@ -1,42 +1,44 @@
 <template>
   <div class="container py-5">
     <h2 class="text-center mb-4">Alur Partner Kompack</h2>
-    <swiper
-      :slides-per-view="width > 992 ? 4 : width > 600 ? 2 : 1"
-      :space-between="25"
-      :scrollbar="{ draggable: true }"
-    >
-      <swiper-slide>
-        <div class="item-slider">
-          <img src="~/assets/image/alur/alur-1.png" alt="alur 1" />
-        </div>
-      </swiper-slide>
-      <swiper-slide>
-        <div class="item-slider">
-          <img src="~/assets/image/alur/alur-2.png" alt="alur 2" />
-        </div> </swiper-slide
-      ><swiper-slide>
-        <div class="item-slider">
-          <img src="~/assets/image/alur/alur-3.png" alt="alur 3" />
-        </div> </swiper-slide
-      ><swiper-slide>
-        <div class="item-slider">
-          <img src="~/assets/image/alur/alur-4.png" alt="alur 4" />
-        </div> </swiper-slide
-      ><swiper-slide>
-        <div class="item-slider">
-          <img src="~/assets/image/alur/alur-6.png" alt="alur 6" />
-        </div> </swiper-slide
-      ><swiper-slide>
-        <div class="item-slider">
-          <img src="~/assets/image/alur/alur-7.png" alt="alur 7" />
-        </div> </swiper-slide
-      ><swiper-slide>
-        <div class="item-slider">
-          <img src="~/assets/image/alur/alur-8.png" alt="alur 8" />
-        </div>
-      </swiper-slide>
-    </swiper>
+    <div class="bg-line">
+      <swiper
+        :slides-per-view="width > 992 ? 4 : width > 600 ? 2 : 1"
+        :space-between="25"
+        :scrollbar="{ draggable: true }"
+      >
+        <swiper-slide>
+          <div class="item-slider">
+            <img src="~/assets/image/alur/alur-1.png" alt="alur 1" />
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="item-slider">
+            <img src="~/assets/image/alur/alur-2.png" alt="alur 2" />
+          </div> </swiper-slide
+        ><swiper-slide>
+          <div class="item-slider">
+            <img src="~/assets/image/alur/alur-3.png" alt="alur 3" />
+          </div> </swiper-slide
+        ><swiper-slide>
+          <div class="item-slider">
+            <img src="~/assets/image/alur/alur-4.png" alt="alur 4" />
+          </div> </swiper-slide
+        ><swiper-slide>
+          <div class="item-slider">
+            <img src="~/assets/image/alur/alur-6.png" alt="alur 6" />
+          </div> </swiper-slide
+        ><swiper-slide>
+          <div class="item-slider">
+            <img src="~/assets/image/alur/alur-7.png" alt="alur 7" />
+          </div> </swiper-slide
+        ><swiper-slide>
+          <div class="item-slider">
+            <img src="~/assets/image/alur/alur-8.png" alt="alur 8" />
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
 
@@ -55,5 +57,11 @@ const { width } = useWindowSize();
 .item-slider {
   display: flex;
   justify-content: center;
+}
+
+.bg-line {
+  background-image: url(../../assets/svg/line-new.svg);
+  background-repeat: no-repeat;
+  background-position: center;
 }
 </style>
